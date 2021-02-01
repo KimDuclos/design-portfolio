b28bbf4509b268e45cc627141e832a5b0fb0aa18
Kim Duclos <kimduclos@gmail.com>
2021-02-01 08:56:45 -0800

    fixed nav, changed about/contact text

--- src/contact.jsx
+++ src/contact.jsx
@@ -9,14 +9,14 @@ const Contact = () => {
         <a href="mailto:kimduclos@gmail.com">kimduclos@gmail.com</a>
       </div>
       <div className="blurb">
-        Always down to collab, open to everything freelance to full-time. I'm
-        hoping to continue digging into the human-web connection. Let's get in
+        Always down to collab, open to everything freelance through full-time. I'm
+        hoping to continue the dig into human-tech connections. Let's get in
         touch.
       </div>
       <div className="social-icons">
-        <SocialIcons icon="./images/github-icon.png" alt="github icon" />
-        <SocialIcons icon="./images/linkedin-icon.png" alt="linkedin icon" />
-        <SocialIcons icon="./images/dribble-icon.png" alt="dribble icon" />
+        <SocialIcons icon="/images/github-icon.png" alt="github icon" />
+        <SocialIcons icon="/images/linkedin-icon.png" alt="linkedin icon" />
+        <SocialIcons icon="/images/dribble-icon.png" alt="dribble icon" />
       </div>
       <p className="copyright 2021 and beyond">Copyright &copy; Kim Duclos</p>
     </div>
